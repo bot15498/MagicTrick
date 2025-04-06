1cd3dc13df95b4bf1b048881a395ba9f660c3ef9
skyler kim <konata.izumi20@gmail.com>
2025-04-06 17:02:53 -0400

    Remove comments and unused files

--- MagicTrick/Assets/Scripts/Deck.cs
+++ MagicTrick/Assets/Scripts/Deck.cs
@@ -6,6 +6,4 @@ public class Deck : MonoBehaviour
 {
     // This is the list of playable cards in this deck. 
     public List<PlayableCard> PlayableCardList;
-    // This is the list of card objects created from the list
-    //public List<Card> Cards;
 }
